65425b854d4a4caa2f0f290e726a8a3ac6f15940
jpramil <julien.pramil@insee.fr>
2023-10-16 10:15:17 +0000

    reorganisation step 2

--- 03_export_data.R
+++
@@ -1,7 +0,0 @@
-aws.s3::s3write_using(
-  x = deces_perturb,
-  FUN = arrow::write_parquet,
-  object = "/appariements/deces_perturb.parquet",
-  bucket = "projet-ssplab",
-  opts = list("region" = "")
-)
